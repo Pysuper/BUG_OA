8efe052639d5900c51536b3c8628048ca097253c
zheng <182176453@qq.com>
2020-08-17 12:46:13 +0800

    完成-中间件对用户信息（额度）的封装

--- middleware/auth.py
+++ middleware/auth.py
@@ -5,20 +5,35 @@
 # @File    : auth.py
 from datetime import datetime
 
-from django.shortcuts import redirect
-from transac.models import Transacation
 from users.models import UserInfo
+from django.shortcuts import redirect
+from transac.models import Transaction, PricePolicy
 from setting.base import WHITE_REGEX_URL_LIST
 from django.utils.deprecation import MiddlewareMixin
+
+
+class Tracer(object):
+    """
+    在中间件中封装tracer对象
+    """
+
+    def __init__(self):
+        self.user = None
+        self.price_policy = None
 
 
 class AuthMiddleware(MiddlewareMixin):
     @staticmethod
     def process_request(request):
+
+        # 实例化一个Tracer()对象
+        request.tracer = Tracer()
+
         # 如果用户登录了, 在request中赋值
         user_id = request.session.get("user_id", 0)
         user_obj = UserInfo.objects.filter(id=user_id).first()
-        request.tracer = user_obj
+
+        request.tracer.user = user_obj
 
         # 白名单：用户没有登录都可以访问的页面
         """
@@ -33,22 +48,38 @@ class AuthMiddleware(MiddlewareMixin):
         if request.path_info in WHITE_REGEX_URL_LIST:
             return
 
-        if not request.tracer:
+        if not request.tracer.user:
             return redirect('/user/login/user/')
 
         # 登录成功之后，进入后台管理时：获取当前用户所拥有的空间额度
+
         # 1. 免费额度在交易记录中存储
-        # 2. 免费额度存储在配置文件中
-
         # 获取用户ID值最大(最近的一次交易记录)
-        transaca_obj = Transacation.objects.filter(user=user_obj, status=2).order('-id').first()
+        _obj = Transaction.objects.filter(user=user_obj, status=2).order_by('-id').first()
 
-        # 判断当前用户的最后交易记录是否过期
-        crrent_datetime = datetime.now()
+        # # 判断当前用户的最后交易记录是否过期
+        current_datetime = datetime.now()
 
-        # 如果交易记录存在，并且交易记录未过期
-        # 没有交易记录的时候，说明当前用户是免费版
-        if transaca_obj.end_datetime and transaca_obj.end_datetime < crrent_datetime:
-            # transaca_obj = Transacation.objects.filter(user=user_obj, status=2).order('id').first()
-            transaca_obj = Transacation.objects.filter(user=user_obj, status=2, price_policy__category=1).first()
-        request.transaca = transaca_obj
+        # # 如果交易记录存在，并且交易记录未过期
+        # # 没有交易记录的时候，说明当前用户是免费版
+        if _obj.end_datetime and _obj.end_datetime < current_datetime:
+            # _obj = Transaction.objects.filter(user=user_obj, status=2).order('id').first()
+            _obj = Transaction.objects.filter(user=user_obj, status=2, pro_policy__category=1).first()
+
+        request.tracer.price_policy = _obj.pro_policy
+
+        # 2. 免费额度存储在配置文件中
+        """
+        transaction_obj = Transaction.objects.filter(user=user_obj, status=2).order_by('-id').first()
+
+        if not transaction_obj:
+            # 没有购买
+            request.price_policy = PricePolicy.objects.filter(category=1, title="个人免费版").first()
+        else:
+            # 付费版: 判断当前用户的最后交易记录是否过期
+            current_datetime = datetime.now()
+            if transaction_obj.end_datetime and transaction_obj.end_datetime < current_datetime:
+                request.price_policy = PricePolicy.objects.filter(category=1, title="个人免费版").first()
+            else:
+                request.price_policy = transaction_obj.price_policy
+        """
