7cb6984a733985b4c103ee4c4c8235fc3ec2e857
zheng <182176453@qq.com>
2020-08-25 17:41:36 +0800

    修改-展示星标、我的、参与

--- apps/project/views.py
+++ apps/project/views.py
@@ -1,5 +1,6 @@
-from django.http import JsonResponse
+from .models import Project, ProjectUser
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.generic.base import View
 from .project_forms import ProjectModelForm
 
@@ -8,7 +9,25 @@ from .project_forms import ProjectModelForm
 class ProjectInfo(View):
     def get(self, request):
         form = ProjectModelForm(request)
-        return render(request, "project/project_list.html", {"form": form})
+
+        # 获取我创建，我参与的项目
+        project_dict = {"star": [], "my": [], "join": []}
+        my_project_list = Project.objects.filter(creator=request.tracer.user)
+        my_join_list = ProjectUser.objects.filter(user=request.tracer.user)
+
+        for row in my_project_list:
+            if row.star:
+                project_dict["star"].append(row)
+            else:
+                project_dict["my"].append(row)
+
+        for item in my_join_list:
+            if item.star:
+                project_dict["star"].append(item.project)
+            else:
+                project_dict["my"].append(item.project)
+
+        return render(request, "project/project_list.html", {"form": form, "project_dict": project_dict})
 
     def post(self, request):
         form = ProjectModelForm(request, data=request.POST)
